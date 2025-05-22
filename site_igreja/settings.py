844dcf75095bfbe24f8ba968cf8dab8574e2ade6
Lucas Albuquerque <lucas.alb2022@gmail.com>
2025-05-22 10:58:39 -0300

    Preparativos para publicar no Render.com

--- site_igreja/settings.py
+++ site_igreja/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-qli58exh12hcsm-lez=s(43i)bn(0uk=tm3a$ou95@xq$o0^er
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -124,3 +125,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True
+)
